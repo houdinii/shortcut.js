663c88d6a9fd4e26c053710f11195343914a7e4c
Brian Barnes <houdinii1984@gmail.com>
2021-04-12 19:18:38 -0600

    modified source to fix a couple small issues for a specific use case. Also refactored to bring more up to date. Added additional license to source code itself.

--- shortcut.js
+++ shortcut.js
@@ -1,14 +1,35 @@
+// noinspection JSDeprecatedSymbols
+
 /**
- * http://www.openjs.com/scripts/events/keyboard_shortcuts/
- * Version : 2.01.B
- * By Binny V A
- * License : BSD
+ * Copyright (C) 2012 by Binny V A http://www.openjs.com/scripts/events/keyboard_shortcuts/
+ * Copyright (C) 2021 by Houdinii (aka Houd1n11) houdinii1985@gmail.com
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee
+ *  is hereby granted.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE
+ *  INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+ *  FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ *  OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ *  OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *  https://github.com/houdinii/shortcut.js
+ *  Version: 2.02.A
+ *  By Houdinii
+ *  License: BSD
+ *
+ *  http://www.openjs.com/scripts/events/keyboard_shortcuts/
+ *  Version : 2.01.B
+ *  By Binny V A
+ *  License : BSD
  */
+
 shortcut = {
-    'all_shortcuts':{},//All the shortcuts are stored in this array
+    //All the shortcuts are stored in this array
+    'all_shortcuts':{},
     'add': function(shortcut_combination,callback,opt) {
         //Provide a set of default options
-        var default_options = {
+        const default_options = {
             'type':'keydown',
             'propagate':false,
             'disable_in_input':false,
@@ -17,43 +38,49 @@ shortcut = {
         }
         if(!opt) opt = default_options;
         else {
-            for(var dfo in default_options) {
+            for(let dfo in default_options) {
                 if(typeof opt[dfo] == 'undefined') opt[dfo] = default_options[dfo];
             }
         }
 
-        var ele = opt.target;
-        if(typeof opt.target == 'string') ele = document.getElementById(opt.target);
-        var ths = this;
+        let ele = opt.target;
+        if(typeof opt.target == 'string') ele = document.getElementById(opt.target.toString());
         shortcut_combination = shortcut_combination.toLowerCase();
-
-        //The function to be called at keypress
-        var func = function(e) {
+        let func = function(e) {
+            // using window.event even though depreciated as it is only used to ensure an event is passed through.
+            // noinspection JSDeprecatedSymbols
             e = e || window.event;
-
-            if(opt['disable_in_input']) { //Don't enable shortcut keys in Input, Textarea fields
-                var element;
+            //Don't enable shortcut keys in Input, Textarea fields if this option is set
+            if(opt['disable_in_input']) {
+                let element;
                 if(e.target) element=e.target;
-                else if(e.srcElement) element=e.srcElement;
-                if(element.nodeType==3) element=element.parentNode;
+                    // Event.srcElement was originally an IE alias and is now depreciated. Suppressed warning. Suppressed
+                // depreciation warning on .srcElement for same reason.
+                else { // noinspection JSUnresolvedVariable,JSDeprecatedSymbols
+                    if(e.window.eventsrcElement) element=e.srcElement;
+                }
+                if(element.nodeType===3) element=element.parentNode;
 
-                if(element.tagName == 'INPUT' || element.tagName == 'TEXTAREA') return;
+                if(element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') return;
             }
 
             //Find Which key is pressed
+            let code;
             if (e.keyCode) code = e.keyCode;
             else if (e.which) code = e.which;
-            var character = String.fromCharCode(code).toLowerCase();
+            let character = String.fromCharCode(code).toLowerCase();
 
-            if(code == 188) character=","; //If the user presses , when the type is onkeydown
-            if(code == 190) character="."; //If the user presses , when the type is onkeydown
+            if(code === 188) character=","; //If the user presses , when the type is onkeydown
+            if(code === 190) character="."; //If the user presses , when the type is onkeydown
 
-            var keys = shortcut_combination.split("+");
-            //Key Pressed - counts the number of valid keypresses - if it is same as the number of keys, the shortcut function is invoked
-            var kp = 0;
+            let keys = shortcut_combination.split("+");
+            // Key Pressed - counts the number of valid key presses
+            // if it is same as the number of keys, the shortcut function is invoked
+            let kp = 0;
 
-            //Work around for stupid Shift key bug created by using lowercase - as a result the shift+num combination was broken
-            var shift_nums = {
+            //Work around for stupid Shift key bug created by using lowercase
+            // as a result the shift+num combination was broken
+            let shift_nums = {
                 "`":"~",
                 "1":"!",
                 "2":"@",
@@ -74,8 +101,9 @@ shortcut = {
                 "/":"?",
                 "\\":"|"
             }
+
             //Special Keys - and their codes
-            var special_keys = {
+            let special_keys = {
                 'esc':27,
                 'escape':27,
                 'tab':9,
@@ -129,7 +157,7 @@ shortcut = {
                 'f12':123
             }
 
-            var modifiers = {
+            let modifiers = {
                 shift: { wanted:false, pressed:false},
                 ctrl : { wanted:false, pressed:false},
                 alt  : { wanted:false, pressed:false},
@@ -141,44 +169,46 @@ shortcut = {
             if(e.altKey)	modifiers.alt.pressed = true;
             if(e.metaKey)   modifiers.meta.pressed = true;
 
-            for(var i=0; k=keys[i],i<keys.length; i++) {
+            let k;
+            for(let i=0; k=keys[i],i<keys.length; i++) {
                 //Modifiers
-                if(k == 'ctrl' || k == 'control') {
+                if(k === 'ctrl' || k === 'control') {
                     kp++;
                     modifiers.ctrl.wanted = true;
 
-                } else if(k == 'shift') {
+                } else if(k === 'shift') {
                     kp++;
                     modifiers.shift.wanted = true;
 
-                } else if(k == 'alt') {
+                } else if(k === 'alt') {
                     kp++;
                     modifiers.alt.wanted = true;
-                } else if(k == 'meta') {
+                } else if(k === 'meta') {
                     kp++;
                     modifiers.meta.wanted = true;
-                } else if(k.length > 1) { //If it is a special key
-                    if(special_keys[k] == code) kp++;
+                } else if(k.length > 1) {
+                    //If it is a special key
+                    if(special_keys[k] === code) kp++;
 
                 } else if(opt['keycode']) {
-                    if(opt['keycode'] == code) kp++;
+                    if(opt['keycode'] === code) kp++;
 
                 } else { //The special keys did not match
-                    if(character == k) kp++;
+                    if(character === k) kp++;
                     else {
                         if(shift_nums[character] && e.shiftKey) { //Stupid Shift key bug created by using lowercase
                             character = shift_nums[character];
-                            if(character == k) kp++;
+                            if(character === k) kp++;
                         }
                     }
                 }
             }
 
-            if(kp == keys.length &&
-                modifiers.ctrl.pressed == modifiers.ctrl.wanted &&
-                modifiers.shift.pressed == modifiers.shift.wanted &&
-                modifiers.alt.pressed == modifiers.alt.wanted &&
-                modifiers.meta.pressed == modifiers.meta.wanted) {
+            if(kp === keys.length &&
+                modifiers.ctrl.pressed === modifiers.ctrl.wanted &&
+                modifiers.shift.pressed === modifiers.shift.wanted &&
+                modifiers.alt.pressed === modifiers.alt.wanted &&
+                modifiers.meta.pressed === modifiers.meta.wanted) {
                 callback(e);
 
                 if(!opt['propagate']) { //Stop the event
@@ -200,22 +230,25 @@ shortcut = {
             'target':ele,
             'event': opt['type']
         };
-        //Attach the function with the event
+        //Attach the function with the event. Note .attachEvent is an IE only thing, suppressed warning
         if(ele.addEventListener) ele.addEventListener(opt['type'], func, false);
-        else if(ele.attachEvent) ele.attachEvent('on'+opt['type'], func);
-        else ele['on'+opt['type']] = func;
+        else { // noinspection JSUnresolvedVariable
+            if(ele.attachEvent) ele.attachEvent('on'+opt['type'], func);
+            else ele['on'+opt['type']] = func;
+        }
     },
 
     //Remove the shortcut - just specify the shortcut and I will remove the binding
     'remove':function(shortcut_combination) {
         shortcut_combination = shortcut_combination.toLowerCase();
-        var binding = this.all_shortcuts[shortcut_combination];
+        let binding = this.all_shortcuts[shortcut_combination];
         delete(this.all_shortcuts[shortcut_combination])
         if(!binding) return;
-        var type = binding['event'];
-        var ele = binding['target'];
-        var callback = binding['callback'];
-
+        let type = binding['event'];
+        let ele = binding['target'];
+        // assist the typecast
+        if (!ele.detachEvent) ele.detachEvent = {};
+        let callback = binding['callback'];
         if(ele.detachEvent) ele.detachEvent('on'+type, callback);
         else if(ele.removeEventListener) ele.removeEventListener(type, callback, false);
         else ele['on'+type] = false;
